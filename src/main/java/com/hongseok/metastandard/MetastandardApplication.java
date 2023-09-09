63e10103aad2610fd1a820557c15296a488b6350
hongseok.oh <hongseok.oh@datastreams.co.kr>
2023-09-09 11:00:15 +0900

    Initial commit

---
+++ src/main/java/com/hongseok/metastandard/MetastandardApplication.java
@@ -0,0 +1,13 @@
+package com.hongseok.metastandard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetastandardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetastandardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hongseok/metastandard/MetastandardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongseok.metastandard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetastandardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
