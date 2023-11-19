aacb19c5fbfbca6dbd04f6c87521bc304c999f49
hongseok.oh <hongseok.oh@datastreams.co.kr>
2023-11-20 01:23:51 +0900

    무중단배포

--- src/main/java/com/hongseok/metastandard/config/auth/SecurityConfig.java
+++ src/main/java/com/hongseok/metastandard/config/auth/SecurityConfig.java
@@ -5,7 +5,28 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig  {
-
+public class SecurityConfig{
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomOAuth2UserService customOAuth2UserService;
+/*
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+    }
+    */
 
 }
